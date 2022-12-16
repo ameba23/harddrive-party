9cd0c0283909e420d46890c3208b10a8b244b2a0
ameba23 <ameba23@systemli.org>
2022-12-16 12:22:30 +0100

    Error handling

--- src/shares.rs
+++ src/shares.rs
@@ -1,10 +1,52 @@
 use async_walkdir::WalkDir;
 use std::path::{Path, PathBuf};
 // use futures_lite::io::AsyncReadExt;
+use crate::messages::response::ls::Entry;
 use futures_lite::stream::StreamExt;
+use sled::IVec;
+use thiserror::Error;
 
 pub const FILES: &[u8; 1] = b"f";
 pub const DIRS: &[u8; 1] = b"d";
+
+/// Error when creating a Shares struct
+#[derive(Error, Debug)]
+pub enum CreateSharesError {
+    #[error(transparent)]
+    IOError(#[from] sled::Error),
+}
+
+/// Error when indexing a dir
+#[derive(Error, Debug)]
+pub enum ScanDirError {
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error("Cannot parse OsString")]
+    OsStringError(),
+    #[error("Unable to merge db record")]
+    DbMergeError(#[from] sled::Error),
+    #[error("Cannot get parent of given dir")]
+    GetParentError,
+}
+
+///Error when parsing a Db entry
+#[derive(Error, Debug)]
+pub enum EntryParseError {
+    #[error("Error parsing UTF8")]
+    Utf8Error(#[from] std::str::Utf8Error),
+    #[error("Error converting database value to u64")]
+    U64ConversionError(),
+}
+
+fn addition_merge(_key: &[u8], old_value: Option<&[u8]>, merged_bytes: &[u8]) -> Option<Vec<u8>> {
+    let old_size = match old_value {
+        Some(v) => u64::from_le_bytes(v.try_into().unwrap_or([0; 8])),
+        None => 0,
+    };
+    let to_add = u64::from_le_bytes(merged_bytes.try_into().unwrap_or([0; 8]));
+    let new_size = old_size + to_add;
+    Some(new_size.to_le_bytes().to_vec())
+}
 
 pub struct Shares {
     files: sled::Tree,
@@ -13,20 +55,21 @@ pub struct Shares {
 }
 
 impl Shares {
-    pub async fn new(storage: impl AsRef<Path>) -> Result<Self, std::io::Error> {
+    pub async fn new(storage: impl AsRef<Path>) -> Result<Self, CreateSharesError> {
         let mut db_dir = storage.as_ref().to_owned();
         db_dir.push("db");
         let db = sled::open(db_dir).expect("open");
-        let files = db.open_tree(FILES).unwrap();
-        let dirs = db.open_tree(DIRS).unwrap();
+        let files = db.open_tree(FILES)?;
+        let dirs = db.open_tree(DIRS)?;
+        dirs.set_merge_operator(addition_merge);
 
         Ok(Shares { files, dirs })
     }
 
-    pub async fn scan(&mut self, root: &str) -> Result<u32, std::io::Error> {
+    pub async fn scan(&mut self, root: &str) -> Result<u32, ScanDirError> {
         let mut added_entries = 0;
         let path = PathBuf::from(root);
-        let share_name = path.file_name().unwrap();
+        let share_name = path.file_name().ok_or_else(|| ScanDirError::GetParentError);
         // self.share_names.insert(share_name, path);
         let mut entries = WalkDir::new(path);
         loop {
@@ -37,15 +80,21 @@ impl Shares {
                         // TODO remove the 'path' portion of the entry, and join it with share_name
                         // can use 'strip_prefix'
                         let path = entry.path();
-                        let filepath = path.clone().into_os_string().into_string().unwrap();
-                        for sub_path in path.ancestors() {
+                        let filepath = path
+                            .clone()
+                            .into_os_string()
+                            .into_string()
+                            .map_err(|_| ScanDirError::OsStringError())?;
+                        let size = metadata.len().to_le_bytes();
+
+                        for sub_path in path.parent().unwrap().ancestors() {
                             println!("sub_path: {:?}", sub_path);
                             // TODO update dirsize
-                            // self.dirs.insert(filepath.as_bytes(), b"sfd").unwrap(),
+                            let sub_path_bytes = sub_path.to_str().unwrap().as_bytes();
+                            self.dirs.merge(sub_path_bytes, size)?;
                         }
-                        let size = metadata.len().to_le_bytes();
-                        // println!("{}", cfg!(target_endian = "big"));
-                        self.files.insert(filepath.as_bytes(), &size).unwrap();
+
+                        self.files.insert(filepath.as_bytes(), &size)?;
                         println!("{:?} {:?}", entry.path(), entry.metadata().await?.is_file());
                         added_entries += 1;
                     }
@@ -60,12 +109,36 @@ impl Shares {
         Ok(added_entries)
     }
 
+    fn kv_to_entry(
+        &self,
+        (name, size): (IVec, IVec),
+        is_dir: bool,
+    ) -> Result<Entry, EntryParseError> {
+        let name = std::str::from_utf8(&name)?;
+        let size = u64::from_le_bytes(
+            size.to_vec()
+                .try_into()
+                .map_err(|_| EntryParseError::U64ConversionError())?,
+        );
+        Ok(Entry {
+            name: name.to_string(),
+            size,
+            is_dir,
+        })
+    }
+
     pub fn prefix_query(&self, prefix: &str) {
-        let prefix_tilde = [prefix, "~"].concat();
-        for kv_result in self.files.range(prefix.as_bytes()..prefix_tilde.as_bytes()) {
+        for kv_result in self.files.scan_prefix(prefix.as_bytes()) {
             let kv = kv_result.unwrap();
-            println!("kvr {:?}", std::str::from_utf8(&kv.0).unwrap());
+            let entry = self.kv_to_entry(kv, false);
+            println!("file {:?}", entry);
             // TODO convert to pathbuf?
+        }
+
+        for kv_result in self.dirs.scan_prefix(prefix.as_bytes()) {
+            let kv = kv_result.unwrap();
+            let entry = self.kv_to_entry(kv, true);
+            println!("dir {:?}", entry);
         }
     }
 }
@@ -81,6 +154,6 @@ mod tests {
         let mut shares = Shares::new(storage).await.unwrap();
         let added = shares.scan("/home/turnip/scrot").await.unwrap();
         println!("added {}", added);
-        shares.prefix_query("/home/turnip/scrot/2022-10-01");
+        shares.prefix_query("");
     }
 }
