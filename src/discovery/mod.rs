5d7700555dd28e27423081e0f8266ea3d3b8a6f2
peg <ameba23@systemli.org>
2023-03-15 09:57:36 +0100

    Better nat type detection

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -1,9 +1,11 @@
 use self::{hole_punch::PunchingUdpSocket, mdns::mdns_server, mqtt::mqtt_client, topic::Topic};
+use anyhow::anyhow;
 use local_ip_address::local_ip;
 use log::debug;
 use quinn::AsyncUdpSocket;
 use rand::Rng;
-use std::net::{IpAddr, SocketAddr};
+use serde::{Deserialize, Serialize};
+use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
 use stunclient::StunClient;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
 
@@ -39,22 +41,10 @@ pub async fn discover_peers(
     let my_local_ip = local_ip()?;
     let raw_socket = tokio::net::UdpSocket::bind(SocketAddr::new(my_local_ip, 0)).await?;
 
-    // Get our public address with STUN
-    let stun_client = StunClient::with_google_stun_server();
-    let public_addr = stun_client
-        .query_external_address_async(&raw_socket)
-        .await?;
+    let (public_addr, nat_type) = stun_test(&raw_socket).await?;
 
     let (socket, hole_puncher) = PunchingUdpSocket::bind(raw_socket).await?;
     let addr = socket.local_addr()?;
-
-    // TODO here we should loop over IPs of all network interfaces
-    let has_nat = addr.ip() != public_addr.ip();
-
-    debug!(
-        "Local address: {:?} Public address {:?} NAT: {}",
-        addr, public_addr, has_nat
-    );
 
     let id = &addr.to_string(); // TODO id should be derived from public key (probably)
 
@@ -70,7 +60,7 @@ pub async fn discover_peers(
             id.to_string(),
             topics,
             public_addr,
-            has_nat,
+            nat_type,
             token,
             peers_tx,
             hole_puncher,
@@ -90,7 +80,45 @@ fn is_private(ip: IpAddr) -> bool {
     }
 }
 
-//
+/// Get our public address and NAT type using STUN
+async fn stun_test(socket: &tokio::net::UdpSocket) -> anyhow::Result<(SocketAddr, NatType)> {
+    // TODO have a list of public stun servers and choose two randomly
+    let stun_client1 = StunClient::with_google_stun_server();
+    let public_addr1 = stun_client1.query_external_address_async(socket).await?;
+    let stun_server = "stun2.l.google.com:19302"
+        .to_socket_addrs()?
+        .find(|x| x.is_ipv4())
+        .ok_or_else(|| anyhow!("Failed to get IP of stun server"))?;
+    let stun_client2 = StunClient::new(stun_server);
+    let public_addr2 = stun_client2.query_external_address_async(socket).await?;
+
+    // TODO here we should loop over IPs of all network interfaces
+    let addr = socket.local_addr()?;
+    let has_nat = addr.ip() != public_addr1.ip();
+    let is_symmetric = public_addr1 != public_addr2;
+
+    let nat_type = if !has_nat {
+        NatType::NoNat
+    } else if is_symmetric {
+        NatType::Symmetric
+    } else {
+        NatType::Asymmetric
+    };
+
+    debug!(
+        "Local address: {:?}  Public address 1: {:?} Public address 2: {:?} NAT: {:?}",
+        addr, public_addr1, public_addr2, nat_type as u8
+    );
+    Ok((public_addr1, nat_type))
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
+pub enum NatType {
+    NoNat = 1,
+    Asymmetric = 2,
+    Symmetric = 3,
+}
+
 // fn addresses_identical(ip1: IpAddr, ip2: IpAddr) -> bool {
 //     ip1 == ip2
 // match ip1 {
