7d4adf1a66e115175e427f72f2a7ef1a51e67f53
peg <ameba23@systemli.org>
2023-04-14 16:44:45 +0200

    Dynamically add/remove topics

--- src/discovery/mqtt.rs
+++ src/discovery/mqtt.rs
@@ -13,7 +13,7 @@ use mqtt::{
 };
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashSet,
+    collections::HashMap,
     net::{SocketAddr, ToSocketAddrs},
     str,
     sync::Arc,
@@ -31,27 +31,27 @@ const TCP_TIMEOUT: Duration = Duration::from_secs(120);
 
 #[derive(Hash, Eq, PartialEq)]
 struct MqttTopic {
-    topic: Topic,
+    // topic: Topic,
     publish_packet: Option<Vec<u8>>,
     subscribe: bool,
 }
 
 impl MqttTopic {
     fn new(
-        topic: Topic,
+        topic: &Topic,
         publish: bool,
         subscribe: bool,
         announce_address: &AnnounceAddress,
         client_id: &str,
     ) -> anyhow::Result<Self> {
         let publish_packet = if publish {
-            Some(create_publish_packet(&topic, &announce_address, client_id)?)
+            Some(create_publish_packet(topic, announce_address, client_id)?)
         } else {
             None
         };
 
         Ok(Self {
-            topic,
+            // topic,
             publish_packet,
             subscribe,
         })
@@ -81,7 +81,9 @@ fn create_publish_packet(
 }
 
 pub struct MqttClient {
-    topics: Arc<Mutex<HashSet<MqttTopic>>>,
+    topics: Arc<Mutex<HashMap<Topic, MqttTopic>>>,
+    client_id: String,
+    announce_address: AnnounceAddress,
 }
 
 impl MqttClient {
@@ -99,34 +101,27 @@ impl MqttClient {
             nat_type,
             token: our_token,
         };
+
         let client_id_clone = client_id.clone();
         let announce_address_clone = announce_address.clone();
-        let topics_set: HashSet<MqttTopic> = topics
-            .into_iter()
-            .filter_map(move |topic| {
-                MqttTopic::new(topic, true, true, &announce_address, &client_id).ok()
-            })
-            .collect();
+
         let mqtt_client = Self {
-            topics: Arc::new(Mutex::new(topics_set)),
+            topics: Default::default(),
+            announce_address: announce_address_clone,
+            client_id: client_id_clone,
         };
 
-        mqtt_client
-            .run(
-                client_id_clone,
-                announce_address_clone,
-                peers_tx,
-                hole_puncher,
-            )
-            .await?;
+        for topic in topics {
+            mqtt_client.add_topic(topic).await?;
+        }
+
+        mqtt_client.run(peers_tx, hole_puncher).await?;
 
         Ok(mqtt_client)
     }
 
     pub async fn run(
         &self,
-        client_id: String,
-        announce_address: AnnounceAddress,
         peers_tx: UnboundedSender<DiscoveredPeer>,
         hole_puncher: HolePuncher,
     ) -> anyhow::Result<()> {
@@ -140,10 +135,10 @@ impl MqttClient {
         let mut stream =
             tokio::time::timeout(TCP_TIMEOUT, TcpStream::connect(server_addr)).await??;
 
-        info!("MQTT Client identifier {:?}", client_id);
+        info!("MQTT Client identifier {:?}", self.client_id);
 
         // Send a connect packet and check the response
-        let mut connect_packet = ConnectPacket::new(client_id.clone());
+        let mut connect_packet = ConnectPacket::new(self.client_id.clone());
         connect_packet.set_clean_session(true); // false?
         connect_packet.set_keep_alive(KEEP_ALIVE);
         let mut connect_packet_buf = Vec::new();
@@ -168,10 +163,10 @@ impl MqttClient {
         {
             let topics = self.topics.lock().await;
             let channel_filters: Vec<(TopicFilter, QualityOfService)> = topics
-                .iter()
-                .map(|t| {
+                .keys()
+                .map(|topic| {
                     (
-                        TopicFilter::new(format!("hdp/{}/#", t.topic.public_id)).unwrap(),
+                        TopicFilter::new(format!("hdp/{}/#", topic.public_id)).unwrap(),
                         QualityOfService::Level0,
                     )
                 })
@@ -206,120 +201,134 @@ impl MqttClient {
 
         // Start a loop processing messages
         let topics_mutex = self.topics.clone();
+        let client_id = self.client_id.clone();
+        let announce_address = self.announce_address.clone();
         tokio::spawn(async move {
             loop {
                 tokio::select! {
-                                Ok(packet) = VariablePacket::parse(&mut stream) => {
-                                    trace!("PACKET {:?}", packet);
+                    Ok(packet) = VariablePacket::parse(&mut stream) => {
+                        trace!("PACKET {:?}", packet);
 
-                                    match packet {
-                                        VariablePacket::PingrespPacket(..) => {
-                                            trace!("Received PINGRESP from broker ..");
-                                        }
-                                        VariablePacket::PublishPacket(ref publ) => {
-                                            if publ.topic_name().ends_with(&client_id) {
-                                                info!("Found our own announce message");
-                                                continue;
-                                            }
-                                            // Find the associated topic
-                                            // TODO handle err
-                                            let topics = topics_mutex.lock().await;
+                        match packet {
+                            VariablePacket::PingrespPacket(..) => {
+                                trace!("Received PINGRESP from broker ..");
+                            }
+                            VariablePacket::PublishPacket(ref publ) => {
+                                if publ.topic_name().ends_with(&client_id) {
+                                    info!("Found our own announce message");
+                                    continue;
+                                }
+                                // Find the associated topic
+                                // TODO handle err
+                                let topics = topics_mutex.lock().await;
 
-                                            if let Some(associated_topic) = topics.iter().find(|&topic| {
-                                                let tn = &publ.topic_name();
-                                                tn.contains(&topic.topic.public_id)
-                                            }) {
-                                                if let Some(remote_peer_announce) = decrypt_using_topic(&publ.payload().to_vec(), &associated_topic.topic) {
-                                                    // TODO dont connect if we are both on the same IP - use mdns
-                                                    // TODO there are more cases when we should not bother hole punching
-                                                    if remote_peer_announce.nat_type != NatType::Symmetric {
-                                                        let mut hole_puncher_clone = hole_puncher.clone();
-                                                        tokio::spawn(async move {
-                                                            info!("Attempting hole punch...");
-                                                            if hole_puncher_clone.hole_punch_peer(remote_peer_announce.public_addr).await.is_err() {
-                                                                warn!("Hole punching failed");
-                                                            } else {
-                                                                info!("Hole punching succeeded");
-                                                            };
-                                                        });
-                                                    };
+                                if let Some(associated_topic) = topics.keys().find(|&topic| {
+                                    let tn = &publ.topic_name();
+                                    tn.contains(&topic.public_id)
+                                }) {
+                                    if let Some(remote_peer_announce) = decrypt_using_topic(&publ.payload().to_vec(), associated_topic) {
+                                        // TODO dont connect if we are both on the same IP - use mdns
+                                        // TODO there are more cases when we should not bother hole punching
+                                        if remote_peer_announce.nat_type != NatType::Symmetric {
+                                            let mut hole_puncher_clone = hole_puncher.clone();
+                                            tokio::spawn(async move {
+                                                info!("Attempting hole punch...");
+                                                if hole_puncher_clone.hole_punch_peer(remote_peer_announce.public_addr).await.is_err() {
+                                                    warn!("Hole punching failed");
+                                                } else {
+                                                    info!("Hole punching succeeded");
+                                                };
+                                            });
+                                        };
 
-                                                    // Decide whether to initiate the connection deterministically
-                                                    // so that only one party initiates
-                                                    let our_nat_badness = announce_address.nat_type as u8;
-                                                    let their_nat_badness = remote_peer_announce.nat_type as u8;
-                                                    let should_initiate_connection = if our_nat_badness == their_nat_badness {
-                                                            // If we both have the same NAT type, use the socket address
-                                                            // as a tie breaker
-                                                            let us = announce_address.public_addr.to_string();
-                                                            let them = remote_peer_announce.public_addr.to_string();
-                                                            us > them
-                                                    } else {
-                                                        // Otherwise the peer with the worst NAT type initiates the
-                                                        // connection
-                                                        our_nat_badness > their_nat_badness
-                                                    };
+                                        // Decide whether to initiate the connection deterministically
+                                        // so that only one party initiates
+                                        let our_nat_badness = announce_address.nat_type as u8;
+                                        let their_nat_badness = remote_peer_announce.nat_type as u8;
+                                        let should_initiate_connection = if our_nat_badness == their_nat_badness {
+                                                // If we both have the same NAT type, use the socket address
+                                                // as a tie breaker
+                                                let us = announce_address.public_addr.to_string();
+                                                let them = remote_peer_announce.public_addr.to_string();
+                                                us > them
+                                        } else {
+                                            // Otherwise the peer with the worst NAT type initiates the
+                                            // connection
+                                            our_nat_badness > their_nat_badness
+                                        };
 
-                                                    if should_initiate_connection {
-                                                        info!("PUBLISH ({})", publ.topic_name());
-                                                        if peers_tx
-                                                            .send(DiscoveredPeer {
-                                                                addr: remote_peer_announce.public_addr,
-                                                                token: remote_peer_announce.token,
-                                                                topic: Some(associated_topic.topic.clone()),
-                                                            })
-                                                            .is_err()
-                                                        {
-                                                            error!("Cannot write to channel");
-                                                            break;
-                                                        }
-                                                    }
-                                                }
+                                        if should_initiate_connection {
+                                            info!("PUBLISH ({})", publ.topic_name());
+                                            if peers_tx
+                                                .send(DiscoveredPeer {
+                                                    addr: remote_peer_announce.public_addr,
+                                                    token: remote_peer_announce.token,
+                                                    topic: Some(associated_topic.clone()),
+                                                })
+                                                .is_err()
+                                            {
+                                                error!("Cannot write to channel");
+                                                break;
                                             }
                                         }
-                                        _ => {}
                                     }
                                 }
-                                // // Send ping packets to keep the connection open
-                                // () = tokio::time::sleep(Duration::from_secs(KEEP_ALIVE as u64 / 2)) => {
-                                //     trace!("Sending PINGREQ to broker");
-                                //
-                                //     let pingreq_packet = PingreqPacket::new();
-                                //
-                                //     let mut pingreq_packet_buf = Vec::new();
-                                //     if pingreq_packet.encode(&mut pingreq_packet_buf).is_err() {
-                                //         error!("Cannot encode MQTT ping packet");
-                                //         break;
-                                //     };
-                                //     if stream.write_all(&pingreq_packet_buf).await.is_err() {
-                                //         error!("Cannot write MQTT ping packet");
-                                //         // break;
-                                //     };
-                                // }
-                                // Send publish packets
-                                () = tokio::time::sleep(Duration::from_secs(KEEP_ALIVE as u64 /2)) => {
-                                    // TODO this should be run immediately as well as after delay
-                                    info!("Sending publish packet");
-                                    let topics = topics_mutex.lock().await;
-                                    // For each topic in the set, send publish message
-                                    for topic in topics.iter() {
-                                        if let Some(publish_packet) = &topic.publish_packet {
-                                        if let Err(e) = stream.write_all(&publish_packet[..]).await {
-                                            error!("Error when writing to mqtt broker {:?}", e);
-                                            break;
-                                    };
                             }
+                            _ => {}
+                        }
+                    }
+                    // // Send ping packets to keep the connection open
+                    // () = tokio::time::sleep(Duration::from_secs(KEEP_ALIVE as u64 / 2)) => {
+                    //     trace!("Sending PINGREQ to broker");
+                    //
+                    //     let pingreq_packet = PingreqPacket::new();
+                    //
+                    //     let mut pingreq_packet_buf = Vec::new();
+                    //     if pingreq_packet.encode(&mut pingreq_packet_buf).is_err() {
+                    //         error!("Cannot encode MQTT ping packet");
+                    //         break;
+                    //     };
+                    //     if stream.write_all(&pingreq_packet_buf).await.is_err() {
+                    //         error!("Cannot write MQTT ping packet");
+                    //         // break;
+                    //     };
+                    // }
+                    // Send publish packets
+                    () = tokio::time::sleep(Duration::from_secs(KEEP_ALIVE as u64 / 2)) => {
+                        // TODO this should be run immediately as well as after delay
+                        info!("Sending publish packets");
+                        let topics = topics_mutex.lock().await;
+                        // For each topic in the set, send publish message
+                        for (_topic, mqtt_topic) in topics.iter() {
+                            if let Some(publish_packet) = &mqtt_topic.publish_packet {
+                                if let Err(e) = stream.write_all(&publish_packet[..]).await {
+                                    error!("Error when writing to mqtt broker {:?}", e);
+                                    break;
+                                };
+                            }
+                        }
+                    }
                 }
-                                }
-                            }
             }
         });
         Ok(())
     }
 
-    pub fn add_topic(&self, _topic: Topic) {}
+    pub async fn add_topic(&self, topic: Topic) -> anyhow::Result<()> {
+        // TODO publish a subscribe message
+        let mqtt_topic =
+            MqttTopic::new(&topic, true, true, &self.announce_address, &self.client_id)?;
+        let mut topics = self.topics.lock().await;
+        topics.insert(topic, mqtt_topic);
+        Ok(())
+    }
 
-    pub fn remove_topic(&self, _topic: Topic) {}
+    pub async fn remove_topic(&self, topic: Topic) -> anyhow::Result<()> {
+        // TODO publish an unsubscribe message
+        let mut topics = self.topics.lock().await;
+        topics.remove(&topic);
+        Ok(())
+    }
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
