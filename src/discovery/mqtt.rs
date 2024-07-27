221dd62528bc2f3ba76fdf2c01c9c4dad94aabfd
peg <ameba23@systemli.org>
2024-07-27 21:31:38 +0200

    Fix mqtt reconnect loop

--- src/discovery/mqtt.rs
+++ src/discovery/mqtt.rs
@@ -295,7 +295,8 @@ impl MqttClient {
                     }
                 };
                 if reconnect {
-                    let mut stream = loop {
+                    tokio::time::sleep(Duration::from_secs(2)).await;
+                    stream = loop {
                         if let Ok(stream) = connect(&server_addr, client_id.clone()).await {
                             break stream;
                         } else {
