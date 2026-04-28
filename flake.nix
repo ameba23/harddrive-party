6e7032cf9f342726fa1e11913cfe092ba6f1c791
peg <ameba23@systemli.org>
2026-04-28 13:58:29 +0200

    Flake

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Nix flake for harddrive-party";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "clippy"
+            "rust-src"
+            "rustfmt"
+          ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+        harddriveParty = rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      in
+      {
+        packages.harddrive-party = harddriveParty;
+        packages.default = harddriveParty;
+        apps.default = flake-utils.lib.mkApp { drv = harddriveParty; };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+            pkgs.binaryen
+            pkgs.firefox
+            pkgs.geckodriver
+            pkgs.trunk
+            pkgs.wasm-bindgen-cli
+          ];
+        };
+      }
+    );
+}
