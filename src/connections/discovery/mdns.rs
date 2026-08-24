aed4b8aeef3de99a9eda76d41f1d13b215708816
peg <ameba23@systemli.org>
2026-08-24 11:05:26 +0200

    Clippy

--- src/connections/discovery/mdns.rs
+++ src/connections/discovery/mdns.rs
@@ -91,7 +91,7 @@ impl MdnsServer {
                         }
                     }
                     ServiceEvent::ServiceRemoved(_type, fullname) => {
-                        debug!("mdns peer removed {:?}", &fullname);
+                        debug!("mdns peer removed {:?}", fullname);
                     }
                     _ => {}
                 }
